416e8f042afe7f2694cc2d7dd93b7b1557814de5
fadielbah <115184115+fadielbah@users.noreply.github.com>
2025-12-17 23:09:03 +0100

    Delete trustworthy column addition script
    Removed script to add trustworthy column to clients table.

--- AgenceLocation/database/run_add_trustworthy.sql
+++ AgenceLocation/database/run_add_trustworthy.sql
@@ -1,11 +1,1 @@
--- ============================================
--- Quick script to add trustworthy column
--- Run this in SQLite to add the column
--- ============================================
-
--- Add trustworthy column to clients table
-ALTER TABLE clients ADD COLUMN trustworthy BOOLEAN DEFAULT 1;
-
--- Check if column was added successfully
-SELECT sql FROM sqlite_master WHERE type='table' AND name='clients';
 
